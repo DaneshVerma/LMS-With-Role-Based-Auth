1584099e4f7306bc01a9508b3aeea37da1ea148b
Danesh <techydanesh@gmail.com>
2025-09-16 21:24:52 +0530

    admin registration

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,5 +1,31 @@
-function registerUser(req, res) {
-  res.json({ message: "ok" });
+const userModel = require("../models/user.model");
+
+async function registerUser(req, res) {
+  const { key } = req.params;
+  if (!key) return res.status(400).json({ message: "Key is required" });
+  const {
+    userName,
+    password,
+    fullName: { firstName, lastName },
+    email,
+  } = req.body;
+  if (!userName || !password || !firstName || !lastName || !email) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
+  if (key === process.env.KEY) {
+    const newUser = await userModel.create({
+      userName,
+      password,
+      fullName: { firstName, lastName },
+      email,
+      role: "admin",
+    });
+    return res
+      .status(201)
+      .json({ message: "admin registered successfully", user: newUser });
+  } else {
+    return res.status(400).json({ message: "Invalid key" });
+  }
 }
 
 module.exports = registerUser;
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema({
   userName: {
@@ -23,4 +24,19 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-module.exports = mongoose.model("user", userSchema);
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (err) {
+    next(err);
+  }
+});
+
+userSchema.methods.comparePassword = async function (candidatePassword) {
+  return await bcrypt.compare(candidatePassword, this.password);
+};
+
+module.exports = mongoose.model("User", userSchema);
